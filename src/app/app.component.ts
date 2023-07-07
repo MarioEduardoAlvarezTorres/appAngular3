0a157eeffc95cd01e9ab000ab193f312a1be2afd
Mario Eduardo <eduardoat44@gmail.com>
2023-07-07 09:08:30 -0600

    uso de pipe data y se cambio a espaniol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Mario Eduardo';
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
+  fecha: Date = new Date();
 
   valorPromesa = new Promise<string>((resolve) => {
     setTimeout(() => {
